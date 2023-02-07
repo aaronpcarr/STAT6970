22a46a6dffe8a46012e929eeba2d8ad5d4feb95b
ddreardon <33428561+ddreardon@users.noreply.github.com>
2023-02-07 14:32:33 -0500

    Modifiy Tokenizer to Drop Punctuations

--- stemmer.py
+++ stemmer.py
@@ -2,12 +2,14 @@ import pandas as pd
 import nltk
 nltk.download('punkt')
 from nltk.stem import PorterStemmer
+from nltk.tokenize import RegexpTokenizer
 
 stemmer = PorterStemmer()
+tokenizer = RegexpTokenizer(r'\w+')
 
 # Having trouble with some of the characters, for now I'm having it ignore the errors but need to look into this later.
 
 comments = pd.read_csv('\STAT6970\BigSix.csv')
 comments['Stemmed Comment'] = comments['Comment'].apply(lambda x: stemmer.stem(x))
-comments['Tokenized Comment'] = comments['Stemmed Comment'].apply(lambda x: nltk.word_tokenize(x))
+comments['Tokenized Comment'] = comments['Stemmed Comment'].apply(lambda x: tokenizer.tokenize(x))
 comments.to_csv("\STAT6970\BigSix_Tokenized.csv")
