b6f53afacc3f888031479f7fce279afad368b760
ddreardon <33428561+ddreardon@users.noreply.github.com>
2023-02-17 10:21:56 -0500

    More robust stemmer; remove punctuation

--- stemmer.py
+++ stemmer.py
@@ -1,15 +1,26 @@
 import pandas as pd
-import nltk
-nltk.download('punkt')
-from nltk.stem import PorterStemmer
-from nltk.tokenize import RegexpTokenizer
+from nltk.tokenize import word_tokenize
+from nltk.stem import SnowballStemmer
+import re
 
-stemmer = PorterStemmer()
-tokenizer = RegexpTokenizer(r'\w+')
+def tokenize(text):
+    text = re.sub(r'[^\w\s]', '', text)
+    tokens = word_tokenize(text)
+    return tokens
+
+stemmer = SnowballStemmer('english')
+
+def stem(tokens):
+    stems = [stemmer.stem(token) for token in tokens]
+    return stems
 
 # Having trouble with some of the characters, for now I'm having it ignore the errors but need to look into this later.
 
 comments = pd.read_csv('\STAT6970\BigSix.csv')
-comments['Stemmed Comment'] = comments['Comment'].apply(lambda x: stemmer.stem(x))
-comments['Tokenized Comment'] = comments['Stemmed Comment'].apply(lambda x: tokenizer.tokenize(x))
+#Remove removed or deleted comments
+comments = comments[~comments['Comment'].isin(['Deleted', 'Removed'])]
+comments['tokenized_comments'] = comments['Comment'].apply(tokenize)
+comments['stemmed_comments'] = comments['tokenized_comments'].apply(stem)
 comments.to_csv("\STAT6970\BigSix_Tokenized.csv")
+
+print(comments['stemmed_comments'])
