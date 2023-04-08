0a0886febd379073106328aebd124de0f5fac439
aaronpcarr <97641961+aaronpcarr@users.noreply.github.com>
2023-04-07 23:27:33 -0400

    Tokenized March and April comments

--- Scripts/stemmer.py
+++ Scripts/stemmer.py
@@ -24,3 +24,22 @@ comments['stemmed_comments'] = comments['tokenized_comments'].apply(stem)
 comments.to_csv("\STAT6970\BigSix_Tokenized.csv", index = False)
 
 print(comments['stemmed_comments'])
+
+comments = pd.read_csv('BigSixMarch.csv')
+#Remove removed or deleted comments
+comments = comments[~comments['Comment'].isin(['Deleted', 'Removed'])]
+comments['tokenized_comments'] = comments['Comment'].apply(tokenize)
+comments['stemmed_comments'] = comments['tokenized_comments'].apply(stem)
+comments.to_csv("BigSixMarch_Tokenized.csv", index = False)
+
+print(comments['stemmed_comments'])
+
+
+comments = pd.read_csv('BigSixApril.csv')
+#Remove removed or deleted comments
+comments = comments[~comments['Comment'].isin(['Deleted', 'Removed'])]
+comments['tokenized_comments'] = comments['Comment'].apply(tokenize)
+comments['stemmed_comments'] = comments['tokenized_comments'].apply(stem)
+comments.to_csv("BigSixApril_Tokenized.csv", index = False)
+
+print(comments['stemmed_comments'])
