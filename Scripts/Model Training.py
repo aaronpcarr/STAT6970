4f20edf7981368e41bb205237ee2bc92907f04ee
aaronpcarr <97641961+aaronpcarr@users.noreply.github.com>
2023-04-14 16:33:06 -0400

    Wrote code for wordclouds for all six teams

--- Scripts/Model Training.py
+++ Scripts/Model Training.py
@@ -34,7 +34,7 @@ ypred = model.predict(comment_test_feature)
 
 from sklearn.metrics import accuracy_score
 accuracy_score(ypred, test_labels)
-print("Log Reg with Comments:", accuracy_score(ypred, test_labels))
+print("Log Reg with Rwplies:", accuracy_score(ypred, test_labels))
 
 misclassified = np.where(test_labels != ypred)
 misclassified
@@ -43,13 +43,18 @@ test_labels[5]
 ypred[5]
 comment_test[5]
 
+coefficients = model.coef_
+feature_names = vectorizer.get_feature_names()
+coef_df = pd.DataFrame(coefficients, columns=feature_names, index=model.classes_)
+coef_df.to_csv("model_coefficients.csv")
+
 from sklearn.tree import DecisionTreeClassifier
 model2 = DecisionTreeClassifier(max_depth=10)
 model2.fit(comment_train_feature, train_labels)
 ypred2 = model2.predict(comment_test_feature)
 
 accuracy_score(ypred2, test_labels)
-print("Decision Tree with Comments:", accuracy_score(ypred2, test_labels))
+print("Decision Tree with Replies:", accuracy_score(ypred2, test_labels))
 
 misclassified2 = np.where(test_labels != ypred2)
 misclassified2
@@ -60,7 +65,7 @@ model3.fit(comment_train_feature, train_labels)
 ypred3 = model3.predict(comment_test_feature)
 
 accuracy_score(ypred3, test_labels)
-print("Random Forest with Comments:", accuracy_score(ypred3, test_labels))
+print("Random Forest with Replies:", accuracy_score(ypred3, test_labels))
 
 misclassified3 = np.where(test_labels != ypred3)
 misclassified3
@@ -78,7 +83,7 @@ model4.fit(comment_train_feature, train_labels)
 ypred4 = model4.predict(comment_test_feature)
 
 accuracy_score(ypred4, test_labels)
-print("GradientBossting with Comments:", accuracy_score(ypred4, test_labels))
+print("GradientBossting with Replies:", accuracy_score(ypred4, test_labels))
 
 misclassified4 = np.where(test_labels != ypred4)
 misclassified4
@@ -111,7 +116,7 @@ ypredr = modelr.predict(commentr_test_feature)
 
 from sklearn.metrics import accuracy_score
 accuracy_score(ypredr, testr_labels)
-print("Log Reg w/o Comments:", accuracy_score(ypredr, testr_labels))
+print("Log Reg w/o Replies:", accuracy_score(ypredr, testr_labels))
 
 from sklearn.tree import DecisionTreeClassifier
 modelr2 = DecisionTreeClassifier(max_depth=10)
@@ -119,7 +124,7 @@ modelr2.fit(commentr_train_feature, trainr_labels)
 ypredr2 = modelr2.predict(commentr_test_feature)
 
 accuracy_score(ypredr2, testr_labels)
-print("Decision Tree w/o Comments:", accuracy_score(ypredr2, testr_labels))
+print("Decision Tree w/o Replies:", accuracy_score(ypredr2, testr_labels))
 
 from sklearn.ensemble import RandomForestClassifier
 modelr3 = RandomForestClassifier(n_estimators = 100, random_state = 1)
@@ -127,7 +132,7 @@ modelr3.fit(commentr_train_feature, trainr_labels)
 ypredr3 = modelr3.predict(commentr_test_feature)
 
 accuracy_score(ypredr3, testr_labels)
-print("RandomForest w/o Comments:", accuracy_score(ypredr3, testr_labels))
+print("RandomForest w/o Replies:", accuracy_score(ypredr3, testr_labels))
 
 from sklearn.ensemble import GradientBoostingClassifier
 modelr4 = GradientBoostingClassifier(
@@ -142,10 +147,4 @@ modelr4.fit(commentr_train_feature, trainr_labels)
 ypredr4 = modelr4.predict(commentr_test_feature)
 
 accuracy_score(ypredr4, testr_labels)
-print("GradientBoosting w/o Comments:", accuracy_score(ypredr4, testr_labels))
-
-
-coefficients = modelr.coef_
-feature_names = vectorizer.get_feature_names_out()
-coef_df = pd.DataFrame(coefficients, columns=feature_names, index=modelr.classes_)
-coef_df.to_csv("model_coefficients.csv")
+print("GradientBoosting w/o Replies:", accuracy_score(ypredr4, testr_labels))
--- Scripts/log reg wordclouds.py
+++ Scripts/log reg wordclouds.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 csv_file = "model_coefficients.csv"
 data = pd.read_csv(csv_file, index_col=0)
 
+#Arsenal
 class_index = 0
 
 feature_names = data.columns.values
@@ -14,13 +15,101 @@ coefficients = data.iloc[class_index].values
 positive_coefficients = {k: v for k, v in zip(feature_names, coefficients) if v > 0}
 positive_normalized_coefficients = {k: v / np.sum(list(positive_coefficients.values())) for k, v in positive_coefficients.items()}
 
-positive_wordcloud = WordCloud(width=800, height = 800, background_color="white", colormap="coolwarm")
+positive_wordcloud = WordCloud(width = 950, height = 950,
+                background_color ='red',
+                colormap= 'Greys')
+
 positive_wordcloud.generate_from_frequencies(positive_normalized_coefficients)
 
 plt.figure(figsize=(10, 10))
 plt.imshow(positive_wordcloud, interpolation="bilinear")
 plt.axis("off")
 plt.show()
+
+#Chelsea
+class_index = 1
+
+feature_names = data.columns.values
+coefficients = data.iloc[class_index].values
+
+positive_coefficients = {k: v for k, v in zip(feature_names, coefficients) if v > 0}
+positive_normalized_coefficients = {k: v / np.sum(list(positive_coefficients.values())) for k, v in positive_coefficients.items()}
+
+positive_wordcloud = WordCloud(width = 950, height = 950,
+                background_color ='blue',
+                colormap= 'Greys')
+positive_wordcloud.generate_from_frequencies(positive_normalized_coefficients)
+
+plt.figure(figsize=(10, 10))
+plt.imshow(positive_wordcloud, interpolation="bilinear")
+
+#Liverpool
+class_index = 2
+
+feature_names = data.columns.values
+coefficients = data.iloc[class_index].values
+
+positive_coefficients = {k: v for k, v in zip(feature_names, coefficients) if v > 0}
+positive_normalized_coefficients = {k: v / np.sum(list(positive_coefficients.values())) for k, v in positive_coefficients.items()}
+
+positive_wordcloud = WordCloud(width = 950, height = 950,
+                background_color ='maroon',
+                colormap= 'BuGn_r')
+positive_wordcloud.generate_from_frequencies(positive_normalized_coefficients)
+
+plt.figure(figsize=(10, 10))
+plt.imshow(positive_wordcloud, interpolation="bilinear")
+
+#Manchester City
+class_index = 3
+
+feature_names = data.columns.values
+coefficients = data.iloc[class_index].values
+
+positive_coefficients = {k: v for k, v in zip(feature_names, coefficients) if v > 0}
+positive_normalized_coefficients = {k: v / np.sum(list(positive_coefficients.values())) for k, v in positive_coefficients.items()}
+
+positive_wordcloud = WordCloud(width = 950, height = 950,
+                background_color ='skyblue',
+                colormap= 'binary')
+positive_wordcloud.generate_from_frequencies(positive_normalized_coefficients)
+
+plt.figure(figsize=(10, 10))
+plt.imshow(positive_wordcloud, interpolation="bilinear")
+
+#Manchester United
+class_index = 4
+
+feature_names = data.columns.values
+coefficients = data.iloc[class_index].values
+
+positive_coefficients = {k: v for k, v in zip(feature_names, coefficients) if v > 0}
+positive_normalized_coefficients = {k: v / np.sum(list(positive_coefficients.values())) for k, v in positive_coefficients.items()}
+
+positive_wordcloud = WordCloud(width = 950, height = 950,
+               background_color ='red',
+                colormap= 'YlOrBr')
+positive_wordcloud.generate_from_frequencies(positive_normalized_coefficients)
+
+plt.figure(figsize=(10, 10))
+plt.imshow(positive_wordcloud, interpolation="bilinear")
+
+#Spurs
+class_index = 5
+
+feature_names = data.columns.values
+coefficients = data.iloc[class_index].values
+
+positive_coefficients = {k: v for k, v in zip(feature_names, coefficients) if v > 0}
+positive_normalized_coefficients = {k: v / np.sum(list(positive_coefficients.values())) for k, v in positive_coefficients.items()}
+
+positive_wordcloud = WordCloud(width = 950, height = 950,
+                background_color ='white',
+                colormap= 'Blues')
+positive_wordcloud.generate_from_frequencies(positive_normalized_coefficients)
+
+plt.figure(figsize=(10, 10))
+plt.imshow(positive_wordcloud, interpolation="bilinear")
 
 negative_coefficients = {k: v for k, v in zip(feature_names, coefficients) if v < 0}
 negative_normalized_coefficients = {k: v / np.sum(list(negative_coefficients.values())) for k, v in negative_coefficients.items()}
